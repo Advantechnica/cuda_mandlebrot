c41b775222a74c8505f94f9479befd93ebefd020
Aaron <Advantechnica@users.noreply.github.com>
2023-02-26 22:55:45 -0800

    Update main.py
    Optimize for 3070 Ti

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numba import cuda
+from numba import cuda, float32
 
 @cuda.jit(device=True)
 def mandelbrot(c_real, c_imag, max_iters):
@@ -14,42 +14,32 @@ def mandelbrot(c_real, c_imag, max_iters):
     return max_iters
 
 @cuda.jit
-def mandelbrot_set(xmin, xmax, ymin, ymax, image, max_iters):
+def mandelbrot_set(xmin, xmax, ymin, ymax, image):
     height, width = image.shape
     pixel_size_x = (xmax - xmin) / width
     pixel_size_y = (ymax - ymin) / height
+
+    # Copy frequently accessed data to shared memory
+    sm_xmin = cuda.shared.array(shape=1, dtype=float32)
+    sm_xmax = cuda.shared.array(shape=1, dtype=float32)
+    sm_ymin = cuda.shared.array(shape=1, dtype=float32)
+    sm_ymax = cuda.shared.array(shape=1, dtype=float32)
+    sm_pixel_size_x = cuda.shared.array(shape=1, dtype=float32)
+    sm_pixel_size_y = cuda.shared.array(shape=1, dtype=float32)
+    tid = cuda.threadIdx.x
+    if tid == 0:
+        sm_xmin[0] = xmin
+        sm_xmax[0] = xmax
+        sm_ymin[0] = ymin
+        sm_ymax[0] = ymax
+        sm_pixel_size_x[0] = pixel_size_x
+        sm_pixel_size_y[0] = pixel_size_y
+    cuda.syncthreads()
+
+    # Copy constant data to constant memory
+    cuda.const.array_like(max_iters)(max_iters)
+
     startX, startY = cuda.grid(2)
     gridX, gridY = cuda.gridsize(2)
     for x in range(startX, width, gridX):
-        real = xmin + x * pixel_size_x
-        for y in range(startY, height, gridY):
-            imag = ymin + y * pixel_size_y
-            image[y, x] = mandelbrot(real, imag, max_iters)
-
-if __name__ == '__main__':
-    # Define the parameters of the Mandelbrot set
-    xmin, xmax, ymin, ymax = -2, 2, -2, 2
-    max_iters = 1000
-    
-    # Define the dimensions of the image to generate
-    width, height = 1024, 1024
-    
-    # Allocate memory on the device for the image
-    image = np.zeros((height, width), dtype=np.uint16)
-    d_image = cuda.to_device(image)
-    
-    # Set up the CUDA kernel
-    threadsperblock = (16, 16)
-    blockspergrid_x = (width + threadsperblock[0] - 1) // threadsperblock[0]
-    blockspergrid_y = (height + threadsperblock[1] - 1) // threadsperblock[1]
-    blockspergrid = (blockspergrid_x, blockspergrid_y)
-    
-    # Generate the Mandelbrot set using CUDA
-    mandelbrot_set[blockspergrid, threadsperblock](xmin, xmax, ymin, ymax, d_image, max_iters)
-    
-    # Copy the image data back to the host and save the image
-    d_image.copy_to_host()
-    import matplotlib.pyplot as plt
-    plt.imshow(image, cmap='gray', extent=[xmin, xmax, ymin, ymax])
-    plt.axis('off')
-    plt.show()
+        real = sm_x
